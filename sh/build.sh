a9d9fbb59a748b47c943bba8c2213aa8e8ac5a9d
魏毅博 <yibo.wei@17zuoye.com>
2021-08-03 18:37:15 +0800

    shell 更新

--- sh/build.sh
+++ sh/build.sh
@@ -1,19 +1,21 @@
 git pull
 workDir=`pwd`
-diffInfo=`git diff --name-only HEAD^`
+
+md5_old=`cat package.md5`
+md5_new=`md5sum package.json |awk '{print $1}'`
 
 if [ ! -d "$workDir/node_modules" ];
 then
-    echo "node_modules目录缺失，开始安装依赖"
+    echo "node_modules目录缺失，开始执行 npm i"
     npm i
-elif [[ $diffInfo =~ "package.json" ]];
+elif [ $md5_old != $md5_new ];
 then
-    echo "package.json文件有变动，开始安装依赖"
+    echo "package.json文件有更新，开始执行 npm i"
     npm i
 else
-    echo "package.json文件无变动，无需安装依赖"
+    echo "package.json文件无更新，无需安装依赖"
 fi
 
 echo "开始构建..."
 npm run build:production
-echo "构建成功>>>>>>>>>>>>>>>>>>"
+echo "构建完成>>>>>>>>>>>>>>>>>>"
--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,18 +1,20 @@
 workDir=`pwd`
-diffInfo=`git diff --name-only HEAD^`
+
+md5_old=`cat package.md5`
+md5_new=`md5sum package.json |awk '{print $1}'`
 
 if [ ! -d "$workDir/node_modules" ];
 then
-    echo "node_modules目录缺失，开始安装依赖"
+    echo "node_modules目录缺失，开始执行 npm i --production"
     npm i --production
-elif [[ $diffInfo =~ "package.json" ]];
+elif [ $md5_old != $md5_new ];
 then
-    echo "package.json文件有变动，开始安装依赖"
+    echo "package.json文件有更新，开始执行 npm i --production"
     npm i --production
 else
-    echo "package.json文件无变动，无需安装依赖"
+    echo "package.json文件无更新，无需安装依赖"
 fi
 
 echo "开始部署..."
 npm run server:production
-echo "部署成功>>>>>>>>>>>>>>>>>>"
+echo "部署完成>>>>>>>>>>>>>>>>>>"
