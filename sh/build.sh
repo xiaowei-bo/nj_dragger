a14cee1d1af9087d6ab62fca8f7160373a96d778
魏毅博 <yibo.wei@17zuoye.com>
2021-08-03 18:55:25 +0800

    sh

--- sh/build.sh
+++ sh/build.sh
@@ -3,7 +3,6 @@ workDir=`pwd`
 
 md5_old=`cat package.md5`
 md5_new=`md5sum package.json | awk '{print $1}'`
-echo "md5_old=$md5_old,md5_new=$md5_new"
 
 if [ ! -d "$workDir/node_modules" ];
 then
@@ -13,6 +12,7 @@ elif [ "$md5_old" = "$md5_new" ];
 then
     echo "package.json文件无更新，无需安装依赖"
 else
+    echo "md5_old=$md5_old,md5_new=$md5_new"
     echo "package.json文件有更新，开始执行 npm i"
     npm i
 fi
--- sh/deploy.sh
+++ sh/deploy.sh
@@ -5,14 +5,15 @@ md5_new=`md5sum package.json |awk '{print $1}'`
 
 if [ ! -d "$workDir/node_modules" ];
 then
-    echo "node_modules目录缺失，开始执行 npm i --production"
-    npm i --production
-elif [ $md5_old != $md5_new ];
+    echo "node_modules目录缺失，开始执行 npm i"
+    npm i
+elif [ "$md5_old" = "$md5_new" ];
 then
-    echo "package.json文件有更新，开始执行 npm i --production"
-    npm i --production
-else
     echo "package.json文件无更新，无需安装依赖"
+else
+    echo "md5_old=$md5_old,md5_new=$md5_new"
+    echo "package.json文件有更新，开始执行 npm i"
+    npm i
 fi
 
 echo "开始部署..."
