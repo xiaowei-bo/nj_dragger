698ecfd1a770d50ba4736c94ceca67066ad6275f
魏毅博 <yibo.wei@17zuoye.com>
2021-08-03 19:04:47 +0800

    s

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -5,16 +5,16 @@ md5_new=`md5sum package.json |awk '{print $1}'`
 
 if [ ! -d "$workDir/node_modules" ];
 then
-    echo "node_modules目录缺失，开始执行 npm i"
-    npm i
+    echo "node_modules目录缺失，开始执行 npm i --production"
+    npm i --production
 elif [ "$md5_old" = "$md5_new" ];
 then
     echo "package.json文件无更新，无需安装依赖"
 else
     echo "md5_old=$md5_old,md5_new=$md5_new"
-    echo "package.json文件有更新，开始执行 npm i"
+    echo "package.json文件有更新，开始执行 npm i --production"
     echo "$md5_new" > package.md5
-    npm i
+    npm i --production
 fi
 
 echo "开始部署..."
