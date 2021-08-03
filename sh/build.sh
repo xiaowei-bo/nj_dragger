de55344c9a9d7180cf6c7745686e14bcf75da8e4
魏毅博 <yibo.wei@17zuoye.com>
2021-08-03 18:53:23 +0800

    sh

--- sh/build.sh
+++ sh/build.sh
@@ -2,19 +2,19 @@ git pull
 workDir=`pwd`
 
 md5_old=`cat package.md5`
-md5_new=`md5sum package.json |awk '{print $1}'`
+md5_new=`md5sum package.json | awk '{print $1}'`
 echo "md5_old=$md5_old,md5_new=$md5_new"
 
 if [ ! -d "$workDir/node_modules" ];
 then
     echo "node_modules目录缺失，开始执行 npm i"
     npm i
-elif [ $md5_old -ne $md5_new ];
+elif [ "$md5_old" = "$md5_new" ];
 then
+    echo "package.json文件无更新，无需安装依赖"
+else
     echo "package.json文件有更新，开始执行 npm i"
     npm i
-else
-    echo "package.json文件无更新，无需安装依赖"
 fi
 
 echo "开始构建..."
