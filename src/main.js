3a8f8a80963bbe58b14eddc7a77ac7dca8018131
魏毅博 <yibo.wei@17zuoye.com>
2021-08-10 10:41:31 +0800

    install

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import store from "./store";
 import App from "./App.vue";
 import ElementUI from "element-ui";
 import NjElement from "@/plugins/index.js";
+import NjDirective from "./directives/index.js";
 import "element-ui/lib/theme-chalk/index.css";
 import "@/style/index.scss";
-import "./directives/index.js";
 import "./permission.js";
 
 Vue.use(ElementUI);
 Vue.use(NjElement);
+Vue.use(NjDirective);
 
 Vue.config.productionTip = false;
 
