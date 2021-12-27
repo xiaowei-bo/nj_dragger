7c21b9e37724330eabb0a99d9f16bdf887f573b3
魏毅博 <15594982127@163.com>
2021-12-27 13:28:30 +0800

    feat: icp备案

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="home">
-        <Header />
+        <Header>
+            <template v-slot:left>
+                <div class="logo-box">
+                    <img class="logo" src="../drag/assets/logo.png" alt="" />
+                </div>
+            </template>
+        </Header>
+        <div id="app-footer">
+            <a href="https://beian.miit.gov.cn" target="_blank">备案号：京ICP备2021040594号</a>
+        </div>
     </div>
 </template>
 
@@ -24,5 +33,18 @@ export default {
 .home{
     width: 100%;
     height: 100%;
+}
+#app-footer{
+    width: 100%;
+    background-color: #000;
+    padding: 20px;
+    text-align: center;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    >a{
+        color: #fff;
+        text-decoration: none;
+    }
 }
 </style>
